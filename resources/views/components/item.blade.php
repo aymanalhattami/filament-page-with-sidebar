985afe8a3de2b1595a71dbd17ef497fc4296fb61
aymanalhattami <ayman.m.alhattami@gmail.com>
2023-10-04 20:20:06 +0300

    fix: code fixes

--- resources/views/components/item.blade.php
+++ resources/views/components/item.blade.php
@@ -15,29 +15,15 @@
     @class([
         'fi-sidebar-item',
         'fi-active fi-sidebar-item-active' => $active,
-    ])
->
-    <a
-            {{ \Filament\Support\generate_href_html($url, $shouldOpenUrlInNewTab) }}
+    ])>
+
+    <a {{ \Filament\Support\generate_href_html($url, $shouldOpenUrlInNewTab) }}
         x-on:click="window.matchMedia(`(max-width: 1024px)`).matches && $store.sidebar.close()"
-        @if (filament()->isSidebarCollapsibleOnDesktop())
-            x-data="{ tooltip: false }"
-            x-effect="
-                tooltip = $store.sidebar.isOpen
-                    ? false
-                    : {
-                          content: @js($slot->toHtml()),
-                          placement: document.dir === 'rtl' ? 'left' : 'right',
-                          theme: $store.theme,
-                      }
-            "
-            x-tooltip.html="tooltip"
-        @endif
         @class([
             'fi-sidebar-item-button relative flex items-center justify-center gap-x-3 rounded-lg px-2 py-2 text-sm outline-none transition duration-75 hover:bg-gray-100 focus:bg-gray-100 dark:hover:bg-white/5 dark:focus:bg-white/5',
             'bg-gray-100 dark:bg-white/5' => $active,
-        ])
-    >
+        ])>
+
         @if (filled($icon))
             <x-filament::icon
                 :icon="($active && $activeIcon) ? $activeIcon : $icon"
@@ -45,22 +31,16 @@
                     'fi-sidebar-item-icon h-6 w-6',
                     'text-gray-400 dark:text-gray-500' => ! $active,
                     'text-primary-600 dark:text-primary-400' => $active,
-                ])
-            />
+                ])/>
+
         @elseif ($grouped)
-            <div
-                class="fi-sidebar-item-grouped-border relative flex h-6 w-6 items-center justify-center"
-            >
+            <div class="fi-sidebar-item-grouped-border relative flex h-6 w-6 items-center justify-center">
                 @if (! $first)
-                    <div
-                        class="absolute -top-1/2 bottom-1/2 w-px bg-gray-300 dark:bg-gray-600"
-                    ></div>
+                    <div class="absolute -top-1/2 bottom-1/2 w-px bg-gray-300 dark:bg-gray-600"></div>
                 @endif
 
                 @if (! $last)
-                    <div
-                        class="absolute -bottom-1/2 top-1/2 w-px bg-gray-300 dark:bg-gray-600"
-                    ></div>
+                    <div class="absolute -bottom-1/2 top-1/2 w-px bg-gray-300 dark:bg-gray-600"></div>
                 @endif
 
                 <div
@@ -68,8 +48,8 @@
                         'relative h-1.5 w-1.5 rounded-full',
                         'bg-gray-400 dark:bg-gray-500' => ! $active,
                         'bg-primary-600 dark:bg-primary-400' => $active,
-                    ])
-                ></div>
+                    ])>
+                </div>
             </div>
         @endif
 
@@ -86,8 +66,7 @@
                 'text-primary-600 dark:text-primary-400' => $active,
                 'font-semibold' => ! $grouped,
                 'font-medium' => $grouped,
-            ])
-        >
+            ])>
             {{ $slot }}
         </span>
 
@@ -98,8 +77,8 @@
                     x-transition:enter="lg:transition lg:delay-100"
                     x-transition:enter-start="opacity-0"
                     x-transition:enter-end="opacity-100"
-                @endif
-            >
+                @endif>
+
                 <x-filament::badge :color="$badgeColor">
                     {{ $badge }}
                 </x-filament::badge>

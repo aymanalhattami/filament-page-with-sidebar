fe91dc4970b8f49a9c7b9fd2082223f00ccff9cf
aymanalhattami <ayman.m.alhattami@gmail.com>
2025-09-03 20:28:04 +0300

    wip: prepare for filament v4

--- resources/views/components/topbar/index.blade.php
+++ resources/views/components/topbar/index.blade.php
@@ -3,7 +3,8 @@
 ])
 
 <div class="mt-8">
-    <nav class="flex items-center gap-x-2 bg-white px-4 py-2 rounded-md dark:bg-gray-900 dark:ring-white/10 md:px-4 lg:px-4 overflow-x-hidden">
+    <nav class="flex items-center gap-x-2 bg-white px-4 py-2 rounded-md dark:bg-gray-900 dark:ring-white/10 md:px-4 lg:px-4 overflow-x-scroll scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-transparent rtl:space-x-reverse">
+         wire:ignore>
         @if ($sidebar->getTitle() !== null || $sidebar->getDescription() != null)
             <div class="me-6 hidden lg:flex flex-col">
                 <h3 class="text-base font-medium text-slate-700 dark:text-white truncate block">
